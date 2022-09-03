46c53bb326eb5f573ce897ba7e885c69922a45a9
Etoimos1 <112579528+Etoimos1@users.noreply.github.com>
2022-09-03 16:48:17 -0300

    Create Exercicio01.cs
    Exercício prático de INF 099

---
+++ Exercicio01.cs
@@ -0,0 +1,14 @@
+namespace INF099
+{
+    public class OlaMundo
+    {
+        static void Main(string[] args) {
+			// Comentário Novo
+            Console.WriteLine("Olá Alunos INF-099 !!!!");
+			Console.WriteLine("BOA NOITE");
+            Console.WriteLine("Alterações com VS Code");
+            // Mais um comentário
+            //
+        }
+    }
+}
